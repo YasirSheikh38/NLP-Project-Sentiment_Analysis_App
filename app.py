1f1a22e1e4615e5ea8b929ebf17eb7c9e893bb81
YasirSheikh38 <iamyasirsheikh38@gmail.com>
2025-06-26 23:10:12 +0500

    Write app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-# from flask import Flask, render_template, request
-# import joblib
-# from utils.preprocess import clean_text
+from flask import Flask, render_template, request
+import joblib
+from utils.preprocess import clean_text
 
-# app = Flask(__name__)
-# model = joblib.load('model/sentiment_model.pkl')
+app = Flask(__name__)
+model = joblib.load('model/sentiment_model.pkl')
 
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     sentiment = ''
-#     if request.method == 'POST':
-#         review = request.form['review']
-#         cleaned = clean_text(review)
-#         sentiment = model.predict([cleaned])[0]
-#     return render_template('index.html', sentiment=sentiment)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    sentiment = ''
+    if request.method == 'POST':
+        review = request.form['review']
+        cleaned = clean_text(review)
+        sentiment = model.predict([cleaned])[0]
+    return render_template('index.html', sentiment=sentiment)
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
